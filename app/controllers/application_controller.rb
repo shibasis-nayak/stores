8ecf8cc9d9c2cc38ca64bbce77b57cb3c81abed5
Shibasis-Nayak <shibasisnayak09@gmail.com>
2026-06-25 18:18:32 +0530

    - Active Storage was installed and configured.  - Profile picture upload functionality was added for users.  - Default profile image support was implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # Changes to the importmap will invalidate the etag for HTML responses
-  stale_when_importmap_changes
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up,
+      keys: [:profile_picture]
+    )
+
+    devise_parameter_sanitizer.permit(
+      :account_update,
+      keys: [:profile_picture]
+    )
+  end
 end
