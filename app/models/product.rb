1e5cf502fb87746420bedc98eecd7968c3efe796
shibasis-nayak <shibasisnayak09@gmail.com>
2026-07-01 12:16:25 +0530

    21-add-stock-column-to-product
     - Add a stock column to Product
     - Add new filter at the top that i can filter out product is in stock or sold out
     - remove uniqueness for description
    
    ---------
    
    Co-authored-by: Shibasis Nayak <shibasisnayak@Shibasiss-MacBook-Pro.local>

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,24 @@
 class Product < ApplicationRecord
   belongs_to :category, optional: true
 
-
   has_many_attached :images
-  validates :name, presence: true, uniqueness: true
-  validates :price, presence: true
-  validates :description, presence: true, uniqueness: true
+
+  validates :name,
+            presence: true,
+            uniqueness: true
+
+  validates :price,
+            presence: true
+
+  validates :description,
+            presence: true
+
+  validates :stock,
+            presence: true,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 0
+            }
 
   def self.ransackable_attributes(auth_object = nil)
     [
@@ -13,9 +26,9 @@ class Product < ApplicationRecord
       "name",
       "description",
       "price",
+      "stock",
       "created_at",
       "updated_at"
     ]
   end
-
 end
