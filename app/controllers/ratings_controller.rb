9cf2bb6497937adcd47bfd71d16e8698ebad5cef
Shibasis Nayak <shibasisnayak09@gmail.com>
2026-07-23 00:55:27 +0530

    -competed feature spec for review and ratings - add validation for -User cannot create a product without a name -User cannot create two products with the same name -User cannot create a product with negative stock -User cannot create a product without price

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,6 +1,5 @@
 class RatingsController < ApplicationController
   before_action :authenticate_user!
-<<<<<<< HEAD
 
   def create
     @product = Product.find(params[:product_id])
@@ -27,32 +26,3 @@ class RatingsController < ApplicationController
     )
   end
 end
-=======
-  
-  def create
-    @product = Product.find(params[:product_id])
-    
-    @rating = Rating.new(rating_params)
-    
-    @rating.user = current_user
-    @rating.product = @product
-    
-    if @rating.save
-      redirect_to @product,
-      notice: "Your rating and review have been submitted successfully."
-    else
-      redirect_to @product,
-      alert: @rating.errors.full_messages.to_sentence
-    end
-  end
-  
-  private
-  
-  def rating_params
-    params.require(:rating).permit(
-    :rating,
-    :review
-    )
-  end
-end 
->>>>>>> e2e4f0f (- debugging)
